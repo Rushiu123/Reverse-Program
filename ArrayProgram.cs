bf12c9a646f7d563de672a5fee81472ac4cdc356
Rushiu123 <rushikesh.ubalkar@gmail.com>
2022-12-17 09:51:57 +0530

    ReverseProgram

---
+++ ArrayProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseProgram
+{
+    internal class ArrayProgram
+    {
+        public static void TestArray()
+        {
+            Console.WriteLine("Please enter size of array");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] number = new int[size];
+            Console.WriteLine("reading input from user and storing into array");
+            for (int i = 0; i < size; i++)
+            {
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Display array elements");
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write(number[i] + " ");
+            }
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ReverseProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter number");
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //ReverseProgram.ReverseNumber(Number);
+            //Console.ReadLine();
+            ReverseProgram.ReverseWord("Rushi");
+            Console.WriteLine("Please enter the word");
+            string word = Console.ReadLine();
+            ReverseProgram.ReverseWord(word);
+            Console.ReadLine();
+        }
+    }
+}
